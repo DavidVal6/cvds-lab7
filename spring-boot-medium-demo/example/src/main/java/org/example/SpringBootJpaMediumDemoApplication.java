e88673a82862ea5a7b651e2bc0d13643dac90a43
DavidVal6 <98176834+DavidVal6@users.noreply.github.com>
2023-03-22 19:57:40 -0500

    p3

--- spring-boot-medium-demo/example/src/main/java/org/example/SpringBootJpaMediumDemoApplication.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-@SpringBootApplication
-public class SpringBootJpaMediumDemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootJpaMediumDemoApplication.class, args);
-    }
-}
--- spring-boot-medium-demo/src/main/java/edu/eci/cvds/springbootmediumdemo/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.eci.cvds.springbootmediumdemo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.eci.cvds.springbootmediumdemo.model.*;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
